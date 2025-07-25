c8a0aa052f129f55016d4d00038a6f8c67ed72ee
faris <farisbrandone@yahoo.com>
2025-07-26 00:20:24 +0100

    updade main.c file

--- main.c
+++ main.c
@@ -3,22 +3,22 @@
 
 int soustraire(int);
 
-int main(int argc, char **arg ){
+int main(){
 
    int i=10;
-   printf("la soustraction de tous les %d entiers  du premier au dernier est egale à %d \n", i, somme(i) );
+   printf("la soustraction de tous les %d entiers  du premier au dernier est egale à %d \n", i, soustraire(i));
 
    return EXIT_SUCCESS;
 
 }
 
-int somme(int i){
+int soustraire(int i){
 
 int resultat=0;
 
  for (int k=0; k<= i; k++){
     resultat-=k;
-    return resultat;
- }
 
+ }
+ return resultat;
 }
