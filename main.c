60e0d1e439fc42caca498b1adb4e4d276aa51087
faris <farisbrandone@yahoo.com>
2025-07-27 09:24:45 +0100

    comment program

--- main.c
+++ main.c
@@ -1,21 +1,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int soustraire(int);
+/**
+ * this program subtracts the first 11 digits from 0 to 10
+ * and display result
+ */
+
+
+ /**
+ * declaration of soustraire function
+ * this function evaluate the successive subtraction of n value with initial value equal 0
+ *  exemple: for three first number, the result return = 0-1-3=-4
+ * @param i represent the latest number to subtracts
+ * @return the result of successive subtraction  of i+1 number
+ * @warning the value 0=< i
+ */
+ int soustraire(int i);
 
 int main(){
-
-   int i=10;
-   printf("la soustraction de tous les %d entiers  du premier au dernier est egale à %d \n", i, soustraire(i));
+   //the  number of value we subtracts
+   int i=11;
+   printf("la soustraction de tous les %d entiers  du premier au dernier est egale à %d \n", i, soustraire(i-1));
 
    return EXIT_SUCCESS;
 
 }
 
-int soustraire(int i){
 
+//definition of soustraire function
+int soustraire(int i){
+ //initial value of result
 int resultat=0;
 
+ //successive subtraction loop
  for (int k=0; k<= i; k++){
     resultat-=k;
 
